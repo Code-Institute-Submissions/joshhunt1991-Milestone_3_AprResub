3fa70109d7652d62a0cfcfbc6ac671e0942a4cda
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-02-22 19:28:30 +0000

    removed unused instance of importing requests

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import requests
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
